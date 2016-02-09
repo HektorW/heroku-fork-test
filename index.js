085eeca86e921fa86a0a3c044551d84089d397bd
Hektor Wallin <hektorw@gmail.com>
2016-02-09 11:24:32 +0100

    Exec blockchain-wallet-service

--- index.js
+++ index.js
@@ -9,3 +9,15 @@ var server = http.createServer(function(req, res) {
 });
 
 server.listen(process.env.PORT || 9000);
+
+//
+// Spawn
+//
+var spawn = require('child_process').spawn;
+var exec = require('child_process').exec;
+
+exec('blockchain-wallet-service start');
+// var service = spawn('blockchain-wallet-service', ['start']);
+// var service = spawn('ls');
+// service.stdout.on('data', console.log.bind(console));
+// service.stderr.on('data', console.log.bind(console));
